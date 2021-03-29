c1f94a8fb7880473d981ed4dfb4026675c28efb2
john.mao <lingzhao.mao@chowbus.com>
2021-03-29 16:58:32 +0800

    Initial Commit

---
+++ MVVPExample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVVPExample
+//
+//  Created by John Mao on 2021/3/29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
